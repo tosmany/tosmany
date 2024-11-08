848dd3160350204bb0e2336234abf93133aa1809
ynamso71 <magdielosmany@gmail.com>
2024-11-08 18:05:56 -0500

    Break responsive biding data home-about

--- src/app/about-section-start/about-section-start.component.ts
+++ src/app/about-section-start/about-section-start.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './about-section-start.component.css'
 })
 export class AboutSectionStartComponent {
+  user =
+    {
+      name: 'Osmany Terry',
+      titre: 'Analyste programmeur',
+      email: 'info@moimeme.com',
+      phone: '+1 (514) 555-5555',
+      dateNaissace: '21 octobre',
+      degree:'CS',
+      web: 'www.moimeme.com',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolorem ipsum iure ullam ipsa expedita libero mollitia non voluptatem tempore quod consectetur unde soluta, qui accusamus quos. Cupiditate, quisquam exercitationem! Aspernatur consequatur, facere numquam ex sed ipsum laboriosam ullam quia ipsa optio atque vero possimus velit dolore quos provident illo excepturi. Repellendus, deserunt. Consequatur perferendis a at fugiat eius nemo.',
+      image: 'osmany-terry.jpg',
+      feelnace: 'Disponible',
+      compentence: {
+        js: '86',
+        php: '66',
+        html: '96',
+        bootstrap: '76',
+      }
+
+
+    }
 
 }
--- src/app/home-section-start/home-section-start.component.ts
+++ src/app/home-section-start/home-section-start.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './home-section-start.component.css'
 })
 export class HomeSectionStartComponent {
-
+  presentation = {
+    gretting:'Salut, mon nom est',
+    name: 'Osmany Terry Diaz',
+    title: 'Développeur web',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur nam beatae provident, voluptatem quibusdam deserunt ipsum accusantium laborum nisi veniam doloremque facere velit consequatur quod dolor inventore. Hic, voluptatibus adipisci?',
+    buttonText: 'En savoir plus',
+    buttonUrl: '/about',
+    image:'../images/avatar_portfolio.jpg'
+  }
 }
