0d2ff755137ee2efaa5f406f963a475500baff7c
Osmany Terry Diaz <tosmany@suncor.com>
2024-11-11 15:36:38 -0500

    Dark-Light theme finishe. Working in routerLink asoide-start component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeSectionStartComponent } from './home-section-start/home-section-start.component';
+import { AboutSectionStartComponent } from './about-section-start/about-section-start.component';
+import { ServiceComponent } from './service/service.component';
+import { PortoFolioSectionComponent } from './porto-folio-section/porto-folio-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeSectionStartComponent  },
+    { path: 'home', component:  HomeSectionStartComponent },
+    { path: 'about', component: AboutSectionStartComponent },
+    { path: 'service', component: ServiceComponent },
+    { path: 'portfolio', component: PortoFolioSectionComponent },
+    { path: 'contact', component: ContactSectionComponent  }
+];
--- src/app/style-switcher/style-switcher.component.ts
+++ src/app/style-switcher/style-switcher.component.ts
@@ -1,27 +1,53 @@
-import { Component, ElementRef, Renderer2, ViewChild,ViewChildren,  QueryList, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+  AfterViewInit,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-
 
 @Component({
   selector: 'app-style-switcher',
   standalone: true,
   templateUrl: './style-switcher.component.html',
-  styleUrls: ['./style-switcher.component.css']
+  styleUrls: ['./style-switcher.component.css'],
 })
 export class StyleSwitcherComponent implements AfterViewInit {
   @ViewChild('styleSwitcher', { static: true }) styleSwitcher!: ElementRef;
-  @ViewChild('styleSwitcherToggle', { static: true }) styleSwitcherToggle!: ElementRef;
-   // Usamos ViewChildren para obtener todas las referencias de los elementos <link> con la clase 'alternate-style'
-   @ViewChildren('alternateStyles') alternateStyles!: QueryList<ElementRef>;
+  @ViewChild('styleSwitcherToggle', { static: true })
+  styleSwitcherToggle!: ElementRef;
+  isDark:boolean = false;
 
-  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+  // Usamos ViewChildren para obtener todas las referencias de los elementos <link> con la clase 'alternate-style'
+  @ViewChildren('alternateStyles') alternateStyles!: QueryList<ElementRef>;
+  // Usamos ViewChild para obtener la referencia del elemento <div> con la clase 'day-night'
+  @ViewChild('dayNight', { static: true }) dayNight!: ElementRef;
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngAfterViewInit(): void {
+    
+    // Solo realizamos las operaciones relacionadas con el DOM si estamos en el navegador
     if (isPlatformBrowser(this.platformId)) {
+      // Al cargar el componente, verificamos si el tema oscuro está activo
+    this.isDark = document.body.classList.contains('dark');
+    this.updateIcon();
       // Toggle style switcher
-      this.renderer.listen(this.styleSwitcherToggle.nativeElement, 'click', () => {
-        this.styleSwitcher.nativeElement.classList.toggle('open');
-      });
+      this.renderer.listen(
+        this.styleSwitcherToggle.nativeElement,
+        'click',
+        () => {
+          this.styleSwitcher.nativeElement.classList.toggle('open');
+        }
+      );
 
       // Hide style switcher on scroll
       window.addEventListener('scroll', () => {
@@ -31,19 +57,44 @@ export class StyleSwitcherComponent implements AfterViewInit {
       });
     }
   }
-
+  /*    ====================   themes colors  =================   */
   setActiveStyle(color: string): void {
-    this.alternateStyles.forEach((styleRef: ElementRef) => {
-      const styleElement = styleRef.nativeElement;
+    const styles = document.querySelectorAll('link.alternate-style');
+    styles.forEach((styleElement: Element) => {
       const styleTitle = styleElement.getAttribute('title');
-
       if (color === styleTitle) {
-        // Si el color seleccionado coincide con el título del archivo CSS, lo habilitamos
         this.renderer.removeAttribute(styleElement, 'disabled');
       } else {
-        // Si no coincide, lo deshabilitamos
         this.renderer.setAttribute(styleElement, 'disabled', 'true');
       }
     });
   }
+
+  /*    ====================   theme light and dark mode  =================   */
+  toggleTheme() {
+    // Alternamos el estado de 'isDark'
+    this.isDark = !this.isDark;
+
+    // Alternamos la clase 'dark' en el cuerpo
+    if (this.isDark) {
+      this.renderer.addClass(document.body, 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark');
+    }
+
+    // Actualizamos el icono de día/noche
+    this.updateIcon();
+  }
+
+  updateIcon() {
+    const icon = this.dayNight.nativeElement.querySelector('i');
+    if (this.isDark) {
+      this.renderer.removeClass(icon, 'fa-moon');
+      this.renderer.addClass(icon, 'fa-sun');
+    } else {
+      this.renderer.removeClass(icon, 'fa-sun');
+      this.renderer.addClass(icon, 'fa-moon');
+    }
+  }
+ 
 }
