066d8377a56b4d0c6384bed270d15381951cb0cb
Osmany Terry Diaz <tosmany@suncor.com>
2024-11-12 15:25:10 -0500

    Adding Typed Animation

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+/* ========================= typing animation ================================ */
+var typed = new Typed(".typing", {
+    strings:["", "Web Analyste", "Developpeur Web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
--- src/app/home-section-start/home-section-start.component.ts
+++ src/app/home-section-start/home-section-start.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home-section-start',
@@ -16,5 +17,17 @@ export class HomeSectionStartComponent {
     buttonText: 'En savoir plus',
     buttonUrl: '/about',
     image:'../images/osmany.jpg'
+  }
+  ngOnInit(): void {
+    // Aquí inicializamos Typed.js
+    const options = {
+      strings: ["", "Web Analyst", "Developper Web"],  // Las cadenas a escribir
+      typeSpeed: 100,  // Velocidad de escritura
+      backSpeed: 60,   // Velocidad de borrado
+      loop: true       // Repetir el ciclo
+    };
+
+    // Crea una nueva instancia de Typed.js y lo aplica al elemento con la clase "typing"
+    new Typed('.typing', options);
   }
 }
