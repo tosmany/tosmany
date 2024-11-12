4cdf48d28bcabc8ec73ce38104ee3217b598ba35
Osmany Terry Diaz <tosmany@suncor.com>
2024-11-12 14:54:57 -0500

    RouterLink finished - starting typing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MainContainerComponent } from './main-container/main-container.component';
-import { AsideStartComponent } from "./aside-start/aside-start.component";
 import { StyleSwitcherComponent } from './style-switcher/style-switcher.component';
-import { ContactSectionComponent } from './contact-section/contact-section.component';
-import { HomeSectionStartComponent } from './home-section-start/home-section-start.component';
-import { PortoFolioSectionComponent } from './porto-folio-section/porto-folio-section.component';
-import { ServiceComponent } from './service/service.component';
-import { MainContentComponent } from './main-content/main-content.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, 
-    RouterModule,
-    RouterLink,
     MainContainerComponent, 
-    AsideStartComponent, 
     StyleSwitcherComponent,
-    MainContentComponent, ServiceComponent,
-    ContactSectionComponent, 
-    HomeSectionStartComponent,
-    PortoFolioSectionComponent],
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/aside-start/aside-start.component.ts
+++ src/app/aside-start/aside-start.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-aside-start',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './aside-start.component.html',
   styleUrl: './aside-start.component.css'
 })
