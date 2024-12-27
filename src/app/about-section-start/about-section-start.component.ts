55df7cc369158302d342a154acf4463f3506deb8
Osmany Terry Diaz <tosmany@suncor.com>
2024-12-27 15:34:18 -0500

    Working on i18n .xlf spanish file

--- src/app/about-section-start/about-section-start.component.ts
+++ src/app/about-section-start/about-section-start.component.ts
@@ -11,15 +11,15 @@ export class AboutSectionStartComponent {
   user =
     {
       name: 'Osmany Terry',
-      titre: 'Analyste programmeur',
+      titre: $localize `Analyste programmeur`,
       email: 'info@moimeme.com',
       phone: '+1 (514) 555-5555',
-      dateNaissace: '21 octobre',
+      dateNaissace: $localize `21 octobre`,
       degree:'CS',
       web: 'www.moimeme.com',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolorem ipsum iure ullam ipsa expedita libero mollitia non voluptatem tempore quod consectetur unde soluta, qui accusamus quos. Cupiditate, quisquam exercitationem! Aspernatur consequatur, facere numquam ex sed ipsum laboriosam ullam quia ipsa optio atque vero possimus velit dolore quos provident illo excepturi. Repellendus, deserunt. Consequatur perferendis a at fugiat eius nemo.',
+      description: $localize `Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolorem ipsum iure ullam ipsa expedita libero mollitia non voluptatem tempore quod consectetur unde soluta, qui accusamus quos. Cupiditate, quisquam exercitationem! Aspernatur consequatur, facere numquam ex sed ipsum laboriosam ullam quia ipsa optio atque vero possimus velit dolore quos provident illo excepturi. Repellendus, deserunt. Consequatur perferendis a at fugiat eius nemo.`,
       image: 'osmany-terry.jpg',
-      feelnace: 'Disponible',
+      feelnace: $localize `Disponible`,
       compentence: {
         js: '86',
         php: '66',
--- src/app/home-section-start/home-section-start.component.ts
+++ src/app/home-section-start/home-section-start.component.ts
@@ -12,11 +12,11 @@ export class HomeSectionStartComponent {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
   presentation = {
-    gretting:'Salut, mon nom est',
+    gretting: $localize `Salut, mon nom est`,
     name: 'Osmany Terry Diaz',
-    title: 'Développeur web',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur nam beatae provident, voluptatem quibusdam deserunt ipsum accusantium laborum nisi veniam doloremque facere velit consequatur quod dolor inventore. Hic, voluptatibus adipisci?',
-    buttonText: 'En savoir plus',
+    title: $localize `Développeur web`,
+    description: $localize `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur nam beatae provident, voluptatem quibusdam deserunt ipsum accusantium laborum nisi veniam doloremque facere velit consequatur quod dolor inventore. Hic, voluptatibus adipisci`,
+    buttonText: $localize `En savoir plus`,
     buttonUrl: '/about',
     image:'../images/osmany.jpg'
   }
@@ -25,7 +25,7 @@ export class HomeSectionStartComponent {
     if (isPlatformBrowser(this.platformId)) {
       // Aquí inicializamos Typed.js
       const options = {
-        strings: ["", "Analyste web", "Développeur web", "Concepteur web"],  // Las cadenas a escribir
+        strings: ["", $localize `Analyste web, Développeur web, Concepteur web`],  // Las cadenas a escribir
         typeSpeed: 100,  // Velocidad de escritura
         backSpeed: 60,   // Velocidad de borrado
         loop: true       // Repetir el ciclo
